35ae669f0d5dcfb1e2e4fda146aa64887089d966
markoj1996 <maki1996marko@gmail.com>
2018-04-03 19:48:16 +0200

    uradjen toolbar za posts

--- app/src/main/java/com/example/marko/projekat/PostsActivity.java
+++ app/src/main/java/com/example/marko/projekat/PostsActivity.java
@@ -3,7 +3,12 @@ package com.example.marko.projekat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class PostsActivity extends AppCompatActivity {
 
@@ -11,22 +16,31 @@ public class PostsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    public void btnStartCreatePostsActivity(View view) {
-        Intent i = new Intent(this,CreatePostActivity.class);
-        startActivity(i);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 
-    public void btnStartReadPostsActivity(View view) {
-        Intent i = new Intent(this,ReadPostActivity.class);
-        startActivity(i);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.add:
+                Toast.makeText(this,"Add new",Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
-    public void btnStartSettingsActivity(View view) {
-        Intent i = new Intent(this,SettingsActivity.class);
-        startActivity(i);
-    }
 
     @Override
     protected void onStart() {
